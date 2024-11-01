40d1c8d726f114937c50157ce0ad2ade41342918
YKDman <dbsrudejr300@gmail.com>
2024-11-01 17:11:41 +0900

    config : tailwind screen color, fonts 적용

---
+++ config/fonts.ts
@@ -0,0 +1,13 @@
+import LocalFont from "next/font/local";
+
+export const NanumBold = LocalFont({
+  src : "../public/fonts/NanumSquareB.ttf",
+  weight: "700",
+  variable : "--font-nanum-bold"
+})
+
+export const NanumRegular = LocalFont({
+  src : "../public/fonts/NanumSquareR.ttf",
+  weight : "400",
+  variable : "--font-nanum-regular"
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,34 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        state : {
+          900 : "#0F172A",
+          800 : "1E293B",
+          500 : "#64748B",
+          400 : "#94A3B8",
+          300 : "#DBD5E1",
+          200 : "#E2E8F0",
+          100 : "#F1F5F9"
+        },
+        violet : {
+          600 : "#7C3AED",
+          100 : "#EDE9FE"
+        },
+        rose : {
+          500 : "#F43F5E"
+        },
+        lime : {
+          300 : "#BEF264"
+        },
+        amber : {
+          800 : "#92400E"
+        }
       },
+      screens : {
+        "web-lg" : "1920px",
+        "web-md" : "744px",
+        "web-sm" : "375px"
+      }
     },
   },
   plugins: [],
